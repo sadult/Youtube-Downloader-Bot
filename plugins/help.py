57babf54f8015fd458b9eb844736790ac3e1bd43
Mersad Shahidi <info@mercads.ir>
2021-02-07 11:27:49 -0800

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Welcome to YT Downloader, I will help you to download any videos in every possible resolution. just send a **Youtobe Link** \n Report any bugs here : @Sadult "
     await message.reply_text(helptxt)
